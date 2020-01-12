cfa9c3b310f41edb5562da9f0718dbe5177734b8
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-01-12 12:48:03 -0500

    dig into payload

--- review-new-issue-action/main.js
+++ review-new-issue-action/main.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
   console.log('Review New Issue: active!');
   const context = JSON.stringify(github.context);
-  console.log(`actor: ${context['actor']}`);
+  console.log(`actor: ${context['payload']['actor']}`);
 } catch(error) {
   core.setFailed(error.message);
 }
