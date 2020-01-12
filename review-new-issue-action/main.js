930fce16b6d1d85dd9d103ce4ee64863128e5fe0
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-01-12 13:48:11 -0500

    try breaking apart code to debug issue

--- review-new-issue-action/main.js
+++ review-new-issue-action/main.js
@@ -5,15 +5,17 @@ try {
   console.log('Review New Issue: active!');
   const payload = JSON.stringify(github.context.payload);
   console.log(`payload: ${payload}`);
-  const actor = github.context.payload.issue.user.login;
-  console.log(`actor: ${actor}`);
 
   const myToken = core.getInput('myToken');
   const octokit = new github.GitHub(myToken);
+  const repoOwner = github.context.repo.owner;
+  const repoName = github.context.repo.repo;
+  const issueId = github.context.payload.issue.id;
+
   octokit.issues.createComment({
-      github.context.repo.owner,
-      github.context.repo.repo,
-      github.context.payload.issue.id,
+      repoOwner,
+      repoName,
+      issueId,
       "*I see you*"
   })
 } catch(error) {
