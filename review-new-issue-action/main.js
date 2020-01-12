9f277ed5430165ebf55db0dc823483dcbf2cebb6
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-01-12 13:05:10 -0500

    try debugging context

--- review-new-issue-action/main.js
+++ review-new-issue-action/main.js
@@ -3,7 +3,8 @@ const github = require('@actions/github');
 
 try {
   console.log('Review New Issue: active!');
-
+  const payload = github.context.payload;
+  console.log(`payload: ${payload}`);
   const actor = github.context.payload.actor;
   console.log(`actor: ${actor}`);
 } catch(error) {
