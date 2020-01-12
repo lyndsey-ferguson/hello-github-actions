9335bb44d474a9690293b0ab72c2a349243a6e3a
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-01-12 14:12:23 -0500

    try putting in async method to handle

--- review-new-issue-action/main.js
+++ review-new-issue-action/main.js
@@ -1,23 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  console.log('Review New Issue: active!');
-
-  const myToken = core.getInput('repo-token');
-  const octokit = new github.GitHub(myToken);
-  const issue = github.context.payload.issue;
-  const issueId = issue.id;
-
-  console.log(`issue id: ${issueId}`);
-
-  octokit.issues.createComment({
-    ...github.context.repo,
-    issue_number: issueId,
-    body: "*I see you*"
-  });
-
-} catch(error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    console.log('Review New Issue: active!');
+  
+    const myToken = core.getInput('repo-token');
+    const octokit = new github.GitHub(myToken);
+    const issue = github.context.payload.issue;
+    const issueId = issue.id;
+  
+    console.log(`issue id: ${issueId}`);
+  
+    octokit.issues.createComment({
+      ...github.context.repo,
+      issue_number: issueId,
+      body: "*I see you*"
+    });
+  
+  } catch(error) {
+    core.setFailed(error.message);
+  }
 }
 
+run();
