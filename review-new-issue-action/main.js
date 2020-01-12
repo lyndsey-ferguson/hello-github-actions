0626d537b89a63b11efab67dcf07c7476976aa99
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2020-01-12 12:45:55 -0500

    try printing actor

--- review-new-issue-action/main.js
+++ review-new-issue-action/main.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
   console.log('Review New Issue: active!');
   const context = JSON.stringify(github.context);
-  console.log(`context: ${context}`);
+  console.log(`actor: ${context['actor']}`);
 } catch(error) {
   core.setFailed(error.message);
 }
